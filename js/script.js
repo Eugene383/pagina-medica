51a638fd9600b4df73aaff5cfce2c9d295362044
Eugene383 <125789251+Eugene383@users.noreply.github.com>
2023-12-07 05:25:55 +0000

    mudancas
    mudanca da lacalização

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let footer = document.getElementById('footerContent')
+let seta = document.getElementById('setaCima')
+
+footer.addEventListener('onscroll',()=>{
+
+    seta.style.display='block'
+    scrol
+
+})
+
